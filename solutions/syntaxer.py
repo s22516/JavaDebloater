9f328754aa3093d6c90ad047ec802528a0249ec8
SoveriegnPihl <marcuspihl01@gmail.com>
2025-11-24 10:37:46 +0100

    added syntaxer

--- solutions/syntaxer.py
+++ solutions/syntaxer.py
@@ -77,8 +77,8 @@ for node in tree_sitter.QueryCursor(method_q).captures(node)["method"]:
     if len(params) != len(methodid.extension.params):
         continue
 
-    # log.debug(methodid.extension.params)
-    # log.debug(params)
+    log.debug(methodid.extension.params)
+    log.debug(params)
 
     for tn, t in zip(methodid.extension.params, params):
         if (tp := t.child_by_field_name("type")) is None:
@@ -94,7 +94,7 @@ else:
     log.warning(f"could not find a method of name {method_name} in {simple_classname}")
     sys.exit(-1)
 
-# log.debug("Found method %s %s", method_name, node.range)
+log.debug("Found method %s %s", method_name, node.range)
 
 body = node.child_by_field_name("body")
 assert body and body.text
@@ -102,17 +102,183 @@ for t in body.text.splitlines():
     log.debug("line: %s", t.decode())
 
 assert_q = tree_sitter.Query(JAVA_LANGUAGE, """(assert_statement) @assert""")
+assert_false_q = tree_sitter.Query(
+    JAVA_LANGUAGE, 
+    """
+      ((assert_statement (false) @assert-false))
+      """)
+assert_true_q = tree_sitter.Query(
+    JAVA_LANGUAGE, 
+    """
+      ((assert_statement (true) @assert-true))
+      """)
 
+divide_by_zero_q = tree_sitter.Query(
+    JAVA_LANGUAGE, 
+    """(binary_expression operator: "/" right: (decimal_integer_literal) @rhs(#eq? @rhs "0")) 
+    @divide_by_zero""")
+
+
+#todo handle a / b cases or 1 / n cases
+divide_by_one_variable_q = tree_sitter.Query(
+    JAVA_LANGUAGE, 
+    """(binary_expression operator: "/" right: (identifier) @rhs) 
+    @divide_by_one_variable""")
+
+
+infinite_loop_q = tree_sitter.Query(
+    JAVA_LANGUAGE,
+    """
+    (
+      (while_statement
+        condition: (parenthesized_expression
+                     (true))
+        body: (_) @loop-body
+      ) @infinite-loop
+    )
+    """)
+
+null_array_decl_q = tree_sitter.Query(
+    JAVA_LANGUAGE,
+    """
+    (
+      (local_variable_declaration
+        declarator: (variable_declarator
+          value: (null_literal) @null-val
+        )
+      ) @null-array-decl
+    )
+    """)
+
+array_access_q = tree_sitter.Query(
+    JAVA_LANGUAGE,
+    """
+    (
+      (array_access
+        array: (identifier) @array-name
+        index: (_) @field_value
+      ) @array-access
+    )
+    """
+)
+
+#todo: handle new int[]
+array_access_with_new_q = tree_sitter.Query(
+    JAVA_LANGUAGE,
+    """
+    (
+      (array_access
+        array: (new_array
+                 type: (identifier) @array-type
+                 size: (_) @array-size
+               ) 
+        index: (_) @field_value
+      ) @array-access-new
+    )
+    """
+)
+
+array_length_q = tree_sitter.Query(
+    JAVA_LANGUAGE,
+    """
+    (
+      (field_access
+        object: (identifier) @array-name
+        field: (identifier) @length-field (#eq? @length-field "length")
+      ) @array-length
+    )
+    """
+)
+
+
+
+
+#return_nothing_q = tree_sitter.Query(
+#    JAVA_LANGUAGE,
+#    """((method_declaration
+#        type: (void_type)
+#        body: (block
+#          (return_statement) @void-return)) @method)"""
+#)
+
+
+
+null_array_decl_found = any(
+    capture_name == "null-array-decl"
+    for capture_name, _ in tree_sitter.QueryCursor(null_array_decl_q).captures(body).items()
+)
+
+array_access_found = any(
+    capture_name == "array-access"
+    for capture_name, _ in tree_sitter.QueryCursor(array_access_q).captures(body).items()
+)
+
+array_length_found = any(
+    capture_name == "array-length"
+    for capture_name, _ in tree_sitter.QueryCursor(array_length_q).captures(body).items()
+)
 
 assert_found = any(
     capture_name == "assert"
     for capture_name, _ in tree_sitter.QueryCursor(assert_q).captures(body).items()
 )
-if assert_found:
+
+assert_false_found = any(
+    capture_name == "assert-false"
+    for capture_name, _ in tree_sitter.QueryCursor(assert_false_q).captures(body).items()
+)
+
+assert_true_found = any(
+    capture_name == "assert-true"
+    for capture_name, _ in tree_sitter.QueryCursor(assert_true_q).captures(body).items()
+)
+
+divide_by_zero_found = any(
+    capture_name == "divide_by_zero"
+    for capture_name, _ in tree_sitter.QueryCursor(divide_by_zero_q).captures(body).items()
+)
+
+divide_by_one_variable_found = any(
+    capture_name == "divide_by_one_variable"
+    for capture_name, _ in tree_sitter.QueryCursor(divide_by_one_variable_q).captures(body).items()
+)
+
+infinite_loop_found = any(
+    capture_name == "infinite-loop"
+    for capture_name, _ in tree_sitter.QueryCursor(infinite_loop_q).captures(body).items()
+)
+
+
+
+if null_array_decl_found and (array_access_found or array_length_found):
+    log.debug("Found null array dereference")
+    print("null pointer;70%")
+else:
+    log.debug("No null array dereference")
+    print("null pointer;0%")
+
+if infinite_loop_found:
+    log.debug("Found infinite loop")
+    print("*;100%")
+else:
+    log.debug("No infinite loop")
+    print("*;0%")
+
+if divide_by_zero_found:
+    log.debug("Found divide by zero")
+    print("divide by zero error;100%")
+else:
+    log.debug("No divide by zero")
+    print("divide by zero error;0%")
+
+if assert_false_found:
+    log.debug("Found false assertion")
+    print("assertion error;100%")
+elif assert_found:
     log.debug("Found assertion")
-    print("assertion error;80%")
+    print("assertion error;50%")
 else:
     log.debug("No assertion")
-    print("assertion error;20%")
+    print("assertion error;0%")
 
 sys.exit(0)
